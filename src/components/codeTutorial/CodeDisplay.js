980efffd15c12a1434571b5e95ecfb6069fdeedc
benskee <benskee33@gmail.com>
2022-06-10 10:20:30 -0400

    CodeDisplay functional component

--- src/components/codeTutorial/CodeDisplay.js
+++ src/components/codeTutorial/CodeDisplay.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { highestValue, getCodeClass } from '../../services/codeDisplayService';
 import Prism from 'prismjs'
 import 'prismjs/components/prism-json';
 import "../../prism.css"
 
-export default class CodeDisplay extends Component {
-    componentDidMount() {
-        Prism.highlightAll();
-    }
-    componentDidUpdate() {
-        Prism.highlightAll();
-    }
+function CodeDisplay(props) {
+    const { selectedFile, currentTime } = props
+    let code = selectedFile.stamps ? selectedFile.stamps[highestValue(selectedFile.stamps, currentTime)]: null
+    
+    useEffect(()=>{
+        Prism.highlightAll()
+    }, [selectedFile, code])
 
-    render() {
-        const { selectedFile, currentTime } = this.props
-        var code = selectedFile.stamps ? selectedFile.stamps[highestValue(selectedFile.stamps, currentTime)]: null
-        return (
-            <div>
-                {code ? <div>
-                    <h3 className='ml-4'>{selectedFile.name} <br/><br/></h3> 
-                    <pre className={`fwm-code-block + ${getCodeClass()}`}><code id='code' className={`language-${selectedFile.fileType}`}>{JSON.parse(selectedFile.stamps[highestValue(selectedFile.stamps, currentTime)])}</code></pre>
-                </div> : <h3 className='ml-4'>Select a file to display.</h3>}
-            </div>
-        )
-    }
+    return (
+        <div>
+            {code ? <div>
+                <h3 className='ml-4'>{selectedFile.name} <br/><br/></h3> 
+                <pre className={`fwm-code-block + ${getCodeClass()}`}><code id='code' className={`language-${selectedFile.fileType}`}>{JSON.parse(code)}</code></pre>
+            </div> : <h3 className='ml-4'>Select a file to display.</h3>}
+        </div>
+    )
 }
+
+export default CodeDisplay
