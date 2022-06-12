1de60a48099e418d7d39183e720421f16f0e545c
benskee <benskee33@gmail.com>
2022-06-12 12:04:46 -0400

    FileDisplay functional component

--- src/components/projects/FileDisplay.js
+++ src/components/projects/FileDisplay.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Files from './Files'
 import { getData } from '../../services/fileDisplayService';
 import IntroProject from './IntroProject';
@@ -6,43 +6,41 @@ import { Paginate } from '../../utils/Paginate'
 import Pagination from '../common/Pagination';
 import Spinner from '../common/Spinner';
 
-export default class FileDisplay extends Component {
-    state = {
-        files: [],
-        pageSize: 5,
-        currentPage: 1,
-        loading: true
-    };
+function FileDisplay(props) {
+    const [allFiles, setAllFiles] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [loading, setLoading] = useState(true);
 
-    async componentDidMount() {
-        let files = await getData()
-        if (files) {this.setState({
-            files: files,
-            loading: false
-        });}
+    const pageSize = 
+
+    useEffect(async () => {
+        let fileData = await getData()
+        if (fileData) {
+            setAllFiles(fileData);
+            setLoading(false);
+        }
+    }, [])
+
+    const handlePageChange = page => {
+        setCurrentPage(page)
     }
 
-    handlePageChange = page => {
-        this.setState({ currentPage: page})
-    }
-    render() {
-        const { currentPage, pageSize, files: allFiles, loading } = this.state 
-
-        const count = allFiles.length
-        const files = Paginate(allFiles, currentPage, pageSize)
-        return (
-            <div>
-                <ul className="row m3">
-                    <IntroProject label='Code' type='code'/>
-                    <IntroProject label='Animation' type='animation'/>
-                    <IntroProject label='Chart Deck' type='chartDeck'/>
-                    {this.state.files.length !== 0 && files.map(file => <Files key={file._id} file={file} user={this.props.user}/>)}
-                    {loading ? <li id='fileDisplaySpinner'>
-                        <Spinner />
-                    </li> : null}
-                </ul>
-                <Pagination itemsCount={count} currentPage={currentPage} pageSize={pageSize} onPageChange={this.handlePageChange}/>
-            </div>
-        )
-    }
+    const count = allFiles.length
+    const files = Paginate(allFiles, currentPage, pageSize)
+    return (
+        <div>
+            <ul className="row m3">
+                <IntroProject label='Code' type='code'/>
+                <IntroProject label='Animation' type='animation'/>
+                <IntroProject label='Chart Deck' type='chartDeck'/>
+                {allFiles.length !== 0 && files.map(file => <Files key={file._id} file={file} user={props.user}/>)}
+                {loading ? <li id='fileDisplaySpinner'>
+                    <Spinner />
+                </li> : null}
+            </ul>
+            <Pagination itemsCount={count} currentPage={currentPage} pageSize={pageSize} onPageChange={handlePageChange}/>
+        </div>
+    )
 }
+
+export default FileDisplay
