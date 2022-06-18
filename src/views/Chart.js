59fe3d5100b26442572f2cb51355c49f38a3f025
benskee <benskee33@gmail.com>
2022-06-17 21:42:06 -0400

    Chart functional component

--- src/views/Chart.js
+++ src/views/Chart.js
@@ -1,39 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player/soundcloud'
 
+function Chart() {
+    const [playedSeconds, setPlayedSeconds] = useState(0)
+    const chartData = require('../JSON/chart.json')
 
-export default class Chart extends Component {
-    constructor() {
-        super();
-        this.state = {
-            chartData: require('../JSON/chart.json'),
-            playedSeconds: 0
-        };
-    }
-    handleProgress = state => {
-        this.setState({
-            playedSeconds: state.playedSeconds
-        });
-        
-        if (!this.state.seeking) {
-            this.setState(state);
-        }
+    const handleProgress = playerState => {
+        setPlayedSeconds(playerState.playedSeconds)
     };
-    render() {
-        const { playedSeconds, chartData } = this.state
-        const currentTime = Math.max.apply(Math, Object.keys(this.state.chartData).filter(function (x) { return x <= playedSeconds; }));
-        const slide = chartData[currentTime]["slide"]
-        const link = chartData[currentTime]["link"]
-        return (
-            <div className="container m-2">
-                <ReactPlayer url="https://soundcloud.com/ben-skee-378863056/chart-deck" onProgress={this.handleProgress} controls />
-                <br/>
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                    <div className='chartImage'>
-                        <img className='img-fluid' src={`./static/chartDeck/${slide}_bitstonker.png`} alt="" />
-                    </div>
-                </a>
-            </div>
-        );
-    }
+
+    const currentTime = Math.max.apply(Math, Object.keys(chartData).filter(function (x) { return x <= playedSeconds; }));
+    const slide = chartData[currentTime]["slide"]
+    const link = chartData[currentTime]["link"]
+    return (
+        <div className="container m-2">
+            <ReactPlayer url="https://soundcloud.com/ben-skee-378863056/chart-deck" onProgress={handleProgress} controls />
+            <br/>
+            <a href={link} target="_blank" rel="noopener noreferrer">
+                <div className='chartImage'>
+                    <img className='img-fluid' src={`./static/chartDeck/${slide}_bitstonker.png`} alt="" />
+                </div>
+            </a>
+        </div>
+    );
 }
+
+export default Chart
